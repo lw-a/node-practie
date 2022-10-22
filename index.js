e49d235411e465dfaf998ba44ed01f6f853bec5e
Adam Hemming <hemming.dev@gmail.com>
2022-10-23 00:04:21 +0900

    connect other pages + 404

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req,res) => {
-  fs.readFile('index.html',function(err,data){
-    if (err) throw err;
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(data);
-    return res.end;
+  if(req.url==='/'){
+    fs.readFile('index.html',function(err,data){
+        if (err) throw err;
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end;
+    });
+}
+else if(req.url==='/about'){
+    fs.readFile('about.html',function(err,data){
+        if (err) throw err;
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end;
+    });
+}
+else if(req.url==='/contact'){
+  fs.readFile('contact.html',function(err,data){
+      if (err) throw err;
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(data);
+      return res.end;
   });
+}
+else{
+  fs.readFile('404.html',function(err,data){
+      if (err) throw err;
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(data);
+      return res.end;
+  });
+}
 }).listen(8080);
